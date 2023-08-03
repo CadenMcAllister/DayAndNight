262288be73b9821d0e9158d4001b6f1bc4660ad7
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-03 12:28:47 -0500

    Scene Transition Finished

--- Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
+++ Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
-    public string scene;
-    void Update(){
-    scene = SceneManager.GetActiveScene().name;
+    public GameObject LevelLoader;
+    public bool Starter = false;
+    public LevelLoader levelLoader;
+    void Awake(){
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
     }
-
     private void OnCollisionEnter2D (Collision2D collision){
-        if (scene == "Main"){
-        }
+        Starter = true;
     }
 }
