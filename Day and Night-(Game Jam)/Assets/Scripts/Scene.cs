5d6ff41e5ce64cb5a4523a61087bce80a174ef67
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-03 08:35:18 -0500

    More Scene Script

--- Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
+++ Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
@@ -3,8 +3,12 @@ using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
     public string scene;
-    void Start(){
-    scene = SceneManager.GetActiveScene().ToString();
-    Debug.Log(scene);
+    void Update(){
+    scene = SceneManager.GetActiveScene().name;
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision){
+        if (scene == "Main"){
+        }
     }
 }
