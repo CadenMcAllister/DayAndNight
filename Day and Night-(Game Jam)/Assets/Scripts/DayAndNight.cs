c54b648d2d6da8f1c47e6e5fe3f7e3c349dd6a2c
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-03 15:26:28 -0500

    Day and Night Completed

--- Day and Night-(Game Jam)/Assets/Scripts/DayAndNight.cs
+++ Day and Night-(Game Jam)/Assets/Scripts/DayAndNight.cs
@@ -7,27 +7,43 @@ public class DayAndNight : MonoBehaviour
     public GameObject Image1;
     public GameObject Image2;
     public GameObject Day;
+
+    private bool isDay = true; 
+
     // Start is called before the first frame update
     void Start()
     {
         Image1 = GameObject.Find("Image");
-        Image2 = GameObject.Find("Image(1)");
+        Image2 = GameObject.Find("Image (1)");
         Night = GameObject.FindGameObjectWithTag("Night");
         Day = GameObject.FindGameObjectWithTag("Day");
         Night.SetActive(false);
         Day.SetActive(true);
+        Image2.SetActive(false); 
+        Image1.SetActive(true); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Night){
-            Image1.SetActive(true);
-            Image2.SetActive(false);
+        if (Input.GetMouseButtonDown(1)){
+            Night.SetActive(true);
+            Day.SetActive(false);
+            isDay = false; 
         }
 
-        if (Day){
+        if (Input.GetMouseButtonUp(1)){
+            Day.SetActive(true);
+            Night.SetActive(false);
+            isDay = true; 
+        }
 
+        if (isDay){ 
+            Image1.SetActive(true);
+            Image2.SetActive(false);
+        } else if (!isDay) { 
+            Image2.SetActive(true);
+            Image1.SetActive(false);
         }
     }
 }
