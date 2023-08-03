043767f387365e845d7d5dce5fee49c65b40f4dc
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-03 11:44:58 -0500

    Brackey's Level Transition

--- Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
+++ Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
@@ -1,14 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
-    public string scene;
-    void Update(){
-    scene = SceneManager.GetActiveScene().name;
-    }
+    public Animator transition;
+    public float transitionTime = 1f;
 
     private void OnCollisionEnter2D (Collision2D collision){
-        if (scene == "Main"){
-        }
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel(){
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator  LoadLevel(int levelIndex){
+        //Play Animation
+        transition.SetTrigger("Start");
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
     }
 }
