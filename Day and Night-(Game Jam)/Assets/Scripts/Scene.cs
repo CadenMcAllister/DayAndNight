ec2154094e6f6b10c5f0b1227800b259952ded62
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-03 11:45:35 -0500

    Revert "Brackey's Level Transition"
    This reverts commit 043767f387365e845d7d5dce5fee49c65b40f4dc.

--- Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
+++ Day and Night-(Game Jam)/Assets/Scripts/Scene.cs
@@ -1,25 +1,14 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
-    public Animator transition;
-    public float transitionTime = 1f;
+    public string scene;
+    void Update(){
+    scene = SceneManager.GetActiveScene().name;
+    }
 
     private void OnCollisionEnter2D (Collision2D collision){
-        LoadNextLevel();
-    }
-
-    public void LoadNextLevel(){
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-
-    IEnumerator  LoadLevel(int levelIndex){
-        //Play Animation
-        transition.SetTrigger("Start");
-        //Wait
-        yield return new WaitForSeconds(transitionTime);
-        //Load Scene
-        SceneManager.LoadScene(levelIndex);
+        if (scene == "Main"){
+        }
     }
 }
